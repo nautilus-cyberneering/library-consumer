3c8057b938f2a6a60fb4dded84fcb3e5efa8cf06
Jose Celano <josecelano@gmail.com>
2022-01-11 15:55:37 +0000

    add metedata to event

--- src/library_consumer/main.py
+++ src/library_consumer/main.py
@@ -8,7 +8,12 @@ app: Typer = typer.Typer()
 
 
 @app.command()
-def create_dummy_event(event_id: str, payload: str):
+def create_dummy_event(
+    event_id: str,
+    payload: str,
+    githun_run_id: str = typer.Argument(None, envvar="GITHUB_RUN_ID"),
+    githun_run_number: str = typer.Argument(None, envvar="GITHUB_RUN_NUMBER"),
+):
     typer.echo("Creating dummy event ...")
     typer.echo("event_id: {event_id}")
     typer.echo("payload: {payload}")
@@ -17,19 +22,24 @@ def create_dummy_event(event_id: str, payload: str):
 
     event = {
         "id": event_id,
+        "name": "dummy",
         "payload": json.loads(payload),
-        "metadata": {}
+        "metadata": {
+            "created_at": {now.timestamp()},
+            "githun_run_id": {githun_run_id},
+            "githun_run_number": {githun_run_number},
+        },
     }
 
-    json_event = json.dumps(event, indent = 4)
+    json_event = json.dumps(event, indent=4)
 
     typer.echo(f"Event: {json_event}")
 
     filepath = f"gitmq/events/{event_id}.json"
 
-    with open(filepath, 'w') as file:
+    with open(filepath, "w") as file:
         file.write(json_event)
-    
+
     typer.echo(f"Created event file: {filepath}")
 
 
