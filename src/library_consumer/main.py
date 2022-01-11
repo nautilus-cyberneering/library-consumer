af03a325d26aaa4157ee31d6b26b20566859d51f
Jose Celano <josecelano@gmail.com>
2022-01-11 13:35:00 +0000

    create dummy event

--- src/library_consumer/main.py
+++ src/library_consumer/main.py
@@ -1,4 +1,6 @@
 import typer
+import datetime
+import json
 from typer.main import Typer
 
 
@@ -6,13 +8,31 @@ app: Typer = typer.Typer()
 
 
 @app.command()
-def create():
-    typer.echo("Creating user: Hiro Hamada")
+def create_dummy_event(event_id: str, payload: str):
+    typer.echo("Creating dummy event ...")
+
+    now = ct = datetime.datetime.now()
+
+    event = {
+        "id": event_id,
+        "payload": json.loads(payload),
+        "metadata": {}
+    }
+
+    json_event = json.dumps(event, indent = 4)
+
+    typer.echo(f"Event: {json_event}")
+
+    filepath = f"gitmq/events/{event_id}.json"
+
+    with open(filepath, 'w') as file:
+        file.write(json_event)
 
 
 @app.command()
-def delete():
-    typer.echo("Deleting user: Hiro Hamada")
+def test():
+    typer.echo("Test")
+
 
 if __name__ == "__main__":
     app()
