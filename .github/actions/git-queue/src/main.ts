bb18ecda83566b31270df98b3edb8ba50af78744
Jose Celano <josecelano@gmail.com>
2022-01-26 13:42:43 +0000

    feat: [#7] remove debug info

--- .github/actions/git-queue/src/main.ts
+++ .github/actions/git-queue/src/main.ts
@@ -12,49 +12,43 @@ async function run(): Promise<void> {
 
     let queue = await Queue.create(inputs.queueName, gitRepoDir, git);
 
-    await core.group(`Debug`, async () => {
-      core.debug(`Git repository directory: ${gitRepoDir}`);
-      core.debug(`Queue messages: ${queue.getMessages()}`);
-    });
-
     switch (inputs.action) {
       case 'create-job':
-
-        // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-        core.debug(`Create job ...`);
-
         const createJobCommit = await queue.dispatch(inputs.jobPayload, false);
 
         await core.group(`Setting outputs`, async () => {
           context.setOutput('job_created', true);
           context.setOutput('job_commit', createJobCommit.hash);
+
+          core.info(`job_created: true`);
+          core.info(`job_commit: ${createJobCommit.hash}`);
         });
 
         break;
       case 'next-job':
-        // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-        core.debug(`Get next job ...`);
-        
         const nextJob = queue.getNextJob();
 
         if (!nextJob.isEmpty()) {
           await core.group(`Setting outputs`, async () => {
             context.setOutput('job_commit', nextJob.commitHash());
             context.setOutput('job_payload', nextJob.payload());
+
+            core.info(`job_commit: ${nextJob.commitHash()}`);
+            core.info(`job_payload: ${nextJob.payload()}`);
           });
         }
 
         break;
 
       case 'mark-job-as-done':
-        // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-        core.debug(`Mark current job as done ...`);
-        
         const markJobAsDoneCommit = await queue.markJobAsDone(inputs.jobPayload, false);
 
         await core.group(`Setting outputs`, async () => {
           context.setOutput('job_created', true);
           context.setOutput('job_commit', markJobAsDoneCommit.hash);
+
+          core.info(`job_created: true`);
+          core.info(`job_commit: ${markJobAsDoneCommit.hash}`);
         });
 
         break;
